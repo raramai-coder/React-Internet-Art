e7c7ac97620fd60514c017b9357b70e5ea56772e
raramai-coder <1846409@students.wits.ac.za>
2023-06-25 21:44:34 +0200

    add three products to shop page

--- internet-art/src/components/Product.js
+++ internet-art/src/components/Product.js
@@ -1,29 +1,38 @@
 import Button from "./Button";
+import PropTypes from "prop-types";
 
-const Product = () => {
+const Product = ({ image, title, description }) => {
+	const imageUrl = require(`../assets/${image}.png`);
+
 	return (
 		<div className="component-container">
 			<div className="product-image-container">
 				<picture>
-					<img
-						src={require("../assets/Rectangle 35.png")}
-						alt="Shop product"
-						className="product-image"
-					/>
+					<img src={imageUrl} alt="Shop product" className="product-image" />
 				</picture>
 			</div>
 			<div className="product-details-container">
-				<h4>Product Name</h4>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua.
-				</p>
+				<h4>{title}</h4>
+				<p>{description}</p>
 				<div className="product-button">
 					<Button label={"Buy"}></Button>
 				</div>
 			</div>
 		</div>
 	);
+};
+
+Product.propTypes = {
+	title: PropTypes.string,
+	image: PropTypes.string,
+	description: PropTypes.string,
+};
+
+Product.defaultProps = {
+	title: "Coffee Mug",
+	image: "product-cup",
+	description:
+		"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
 };
 
 export default Product;
--- internet-art/src/pages/Shop.js
+++ internet-art/src/pages/Shop.js
@@ -2,15 +2,22 @@ import NavBar from "../components/NavBar";
 import Header from "../components/Header";
 import Product from "../components/Product";
 
+import data from "../data.json";
+
 const Shop = () => {
 	return (
 		<div>
 			<NavBar />
 			<Header title="Shop" />
 			<div className="product-holder">
-				<Product />
-				<Product />
-				<Product />
+				{data.products.map((product, index) => (
+					<Product
+						image={product.image}
+						title={product.title}
+						description={product.description}
+						key={index}
+					/>
+				))}
 			</div>
 		</div>
 	);
